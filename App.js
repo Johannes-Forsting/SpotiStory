89adb510a588731594619fadbb70bfa705d0656c
Johannes-Forsting <johannes.forsting@gmail.com>
2023-05-02 11:17:33 +0200

    Can send a request and get a access token with client secret and client id as environment variables

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {CLIENT_SECRET, CLIENT_ID} from '@env';
+
 export default function App() {
+  const [Data, setData] = useState(null);
+  const [Token, setToken] = useState(null);
+
+    useEffect(() => {
+        async function getToken(){
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET
+            };
+            const getTokenData = await fetch("https://accounts.spotify.com/api/token", options)
+                .then(res => res.json())
+                .catch(error => console.error(error));
+            setToken(getTokenData);
+        }
+        getToken();
+
+    }, [])
+
+
+  /*useEffect(() => {
+    async function fetchData() {
+      const dataFromAPI = await fetch("https://randomuser.me/api/")
+          .then(res => res.json())
+          .catch(error => console.error(error));
+      setData(dataFromAPI);
+    }
+    fetchData();
+  }, []);*/
+
   return (
-    <View style={styles.container}>
-      <Text>SpotiStory cumming to your appstore soon</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        {/*<Text>Random person from API: </Text>
+        <Text>{Data ? Data.results[0].name.title : 'Loading...'}</Text>
+        <Text>{Data ? Data.results[0].name.first : 'Loading...'}</Text>
+        <Text>{Data ? Data.results[0].name.last : 'Loading...'}</Text>*/}
+        <Text>{Token ? Token.access_token : 'Loading Token...'}</Text>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-  };
+  presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
+  plugins: [
+    [
+      'babel-plugin-dotenv-import',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
+  }
 };
